8e315d2ed3aaf9296decd72a90deb21f4ce17caf
Gloria R <norimaqidoe@hotmail.com>
2025-09-02 21:09:20 -0600

    feat: add escape close popup function

--- scripts/index.js
+++ scripts/index.js
@@ -53,12 +53,23 @@ initialCards.forEach((cardData) => {
   cardsList.prepend(createCard(cardData));
 });
 
+function handleEscClose(evt) {
+  if (evt.key === "Escape") {
+    const openedPopup = document.querySelector(".popup_opened");
+    if (openedPopup) {
+      closePopup(openedPopup);
+    }
+  }
+}
+
 function openPopup(popup) {
   popup.classList.add("popup_opened");
+  document.addEventListener("keydown", handleEscClose);
 }
 
 function closePopup(popup) {
   popup.classList.remove("popup_opened");
+  document.removeEventListener("keydown", handleEscClose);
 }
 
 function handleOverlayClick(evt) {
