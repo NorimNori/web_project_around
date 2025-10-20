ebd7fbdfad1ed0d978e2ea7c6decdea59306b553
Gloria R <norimaqidoe@hotmail.com>
2025-10-20 00:06:20 -0600

    feat: start Api class with private methods

---
+++ scripts/Api.js
@@ -0,0 +1,17 @@
+class Api {
+  constructor({ baseUrl, headers }) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
+
+  _checkResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Error: ${res.status}`);
+  }
+
+  _handleError(error) {
+    console.log(error);
+  }
+}
